03f29682e7c36563dc1e596a752e77305aa4e9f7
akalp <hasan.akalp@gmail.com>
2026-03-02 02:47:08 +0300

    fix(tap): correct caffbar release URL

--- Casks/caffbar.rb
+++ Casks/caffbar.rb
@@ -2,7 +2,7 @@ cask "caffbar" do
   version "0.1.1"
   sha256 "938b1b0014fb8a57f3c9698589e1dca7344e3d91ad548833d681e1e144bd8f88"
 
-  url "https://github.com/akalp/caffbar/releases/download/v0.1.0/CaffBar-0.1.0.zip"
+  url "https://github.com/akalp/caffbar/releases/download/v0.1.1/CaffBar-0.1.1.zip"
   name "CaffBar"
   desc "Menu bar utility to control caffeinate with presets and modes"
   homepage "https://github.com/akalp/caffbar"
